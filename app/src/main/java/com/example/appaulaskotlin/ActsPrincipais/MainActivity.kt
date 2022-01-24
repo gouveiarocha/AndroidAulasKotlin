074ade63f0552692d8c446d1c34d835751f26208
gouveiarocha <gouveia.rocha@hotmail.com>
2022-01-24 14:36:35 -0300

    add study codes

--- app/src/main/java/com/example/appaulaskotlin/ActsPrincipais/MainActivity.kt
+++ app/src/main/java/com/example/appaulaskotlin/ActsPrincipais/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         startActivity(Intent(this, MVVM::class.java))
+        //novo teste
 
     }
 }
