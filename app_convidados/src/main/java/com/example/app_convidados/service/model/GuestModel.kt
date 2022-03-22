c821c351c87c6d1cbc7ccb43f1203bdc5e5dc16c
gouveiarocha <gouveia.rocha@hotmail.com>
2022-03-22 12:34:24 -0300

    start db room implementation

--- app_convidados/src/main/java/com/example/app_convidados/service/model/GuestModel.kt
+++ app_convidados/src/main/java/com/example/app_convidados/service/model/GuestModel.kt
@@ -1,3 +1,28 @@
 package com.example.app_convidados.service.model
 
-class GuestModel(val id: Int = 0, var name: String, var presence: Boolean)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// obs: no curso, essa classe é alterada para uso da lib room, após a conclusão do app convidados
+// a implementação original abordada no curso utilizava uma classe database helper para manipular
+// os dados e salvar no db
+
+// Entidade
+@Entity(tableName = "Guest")
+class GuestModel{
+
+    @PrimaryKey(autoGenerate = true) //define como chave primaria e autoincremento
+    @ColumnInfo(name = "id") //define o nome da coluna, se não colocar, usa o nome da varivel
+    var id: Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presense: Boolean = true
+
+    //val id: Int = 0, var name: String, var presence: Boolean
+
+
+}
