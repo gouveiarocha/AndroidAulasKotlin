d07e959ccecb831bf8b5441bde5da159cd9305bd
gouveiarocha <gouveia.rocha@hotmail.com>
2022-07-06 02:09:55 -0300

    App 'Tasks' - Udemy Android com Kotlin - Implementação retrofit client e inicio de implementação das requisicoes API para Login.

--- Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,8 +2,11 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPersonRepository = PersonRepository(application)
 
     /** Verifica se usuário está logado */
     fun verifyLoggedUser() {
@@ -12,7 +15,7 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     /** Faz login usando API */
     fun doLogin(email: String, password: String) {
-
+        mPersonRepository.login(email, password)
     }
 
 }
