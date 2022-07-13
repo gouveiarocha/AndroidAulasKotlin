c17285cc2d978ba1cdf8c9c6a398d25f90abe025
gouveiarocha <gouveia.rocha@hotmail.com>
2022-07-13 11:36:38 -0300

    Gravando dados do Header com SharedPreferences e adicionandos headers na requisição da API.

--- Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,6 +1,9 @@
 package com.devmasterteam.tasks.service.repository.remote
 
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,23 +11,47 @@ class RetrofitClient private constructor() {
 
     // Singleton.
     companion object {
+
         private lateinit var INSTANCE: Retrofit
+        private var token: String = ""
+        private var personKey: String = ""
+
         private fun getRetrofitInstance(): Retrofit {
+
+            val httpClient = OkHttpClient.Builder()
+
+            httpClient.addInterceptor { chain ->
+                val request = chain.request() //pega a requisição e barra ela.
+                    .newBuilder() //adiciono o que eu preciso.
+                    .addHeader(TaskConstants.HEADER.TOKEN_KEY, token)
+                    .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey)
+                    .build() //faço novamente o build.
+                chain.proceed(request) //libero para seguir.
+            }
+
             if (!::INSTANCE.isInitialized) {
                 synchronized(RetrofitClient::class) { //evitar problemas com multi-threads.
                     INSTANCE = Retrofit.Builder()
                         .baseUrl("http://devmasterteam.com/CursoAndroidAPI/") //url base API.
-                        .client(OkHttpClient.Builder().build()) //cliente http.
+                        .client(httpClient.build()) //cliente http.
                         .addConverterFactory(GsonConverterFactory.create()) //conversor json.
                         .build()
                 }
             }
+
             return INSTANCE
+
         }
 
         // Get com implementação de classe genérica, assim podemos criar serviço com qualquer classe.
         fun <S> getService(serviceClass: Class<S>): S{
             return getRetrofitInstance().create(serviceClass)
+        }
+
+        // Recebe os headers e adiciona nas variaveis.
+        fun addHeaders(tokenValue: String, personKeyValue: String){
+            token = tokenValue
+            personKey = personKeyValue
         }
 
     }
--- Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -4,14 +4,18 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
 
     private val _login = MutableLiveData<ValidationModel>()
     var login: LiveData<ValidationModel> = _login
@@ -25,12 +29,20 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     fun doLogin(email: String, password: String) {
         personRepository.login(email, password, object : APIListener<PersonModel>{
             override fun onSuccess(result: PersonModel) {
+                saveHeaders(result)
+                RetrofitClient.addHeaders(result.token, result.personKey)
                 _login.value = ValidationModel()
             }
             override fun onFailure(message: String) {
                 _login.value = ValidationModel(message)
             }
         })
+    }
+
+    private fun saveHeaders(result: PersonModel) {
+        securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
+        securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
+        securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
     }
 
 }
