ba38d98b976218fffd895a5a8f3a5b7a69150dc6
gouveiarocha <gouveia.rocha@hotmail.com>
2022-07-28 12:31:22 -0300

    Refatoração para criar uma BaseRepository com o método de conversão para json que é comum entre utilização de todos os demais repositorios. Otimização do código e uso da Herança.

---
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -0,0 +1,11 @@
+package com.devmasterteam.tasks.service.repository
+
+import com.google.gson.Gson
+
+open class BaseRepository {
+
+    fun failReponseFromJson(jsonString: String): String {
+        return Gson().fromJson(jsonString, String::class.java)
+    }
+
+}
