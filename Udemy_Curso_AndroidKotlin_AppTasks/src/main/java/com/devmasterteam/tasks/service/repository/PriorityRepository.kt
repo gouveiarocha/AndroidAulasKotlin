39598f18094d1526eb0c746c7951cd959fbe06d8
gouveiarocha <gouveia.rocha@hotmail.com>
2022-08-04 12:55:37 -0300

    Refatoração e melhorias nos nosso repositorios. Extração de métodos repetitivos.

--- Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -11,7 +11,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(val context: Context) : BaseRepository() {
+class PriorityRepository(context: Context) : BaseRepository(context) {
 
     companion object {
         private val cache = mutableMapOf<Int, String>()
@@ -40,20 +40,7 @@ class PriorityRepository(val context: Context) : BaseRepository() {
     }
 
     fun list(listener: APIListener<List<PriorityModel>>) {
-        val call = remote.list()
-        call.enqueue(object : Callback<List<PriorityModel>> {
-            override fun onResponse(
-                call: Call<List<PriorityModel>>,
-                response: Response<List<PriorityModel>>
-            ) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-
-        })
+        executeCall(remote.list(), listener)
     }
 
     fun save(list: List<PriorityModel>) {
--- Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
+++ Udemy_Curso_AndroidKotlin_AppTasks/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -10,67 +10,29 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class TaskRepository(val context: Context) : BaseRepository() {
+class TaskRepository(context: Context) : BaseRepository(context) {
 
     private val remote = RetrofitClient.getService(TaskService::class.java)
 
     fun list(listener: APIListener<List<TaskModel>>) {
-        val call = remote.list()
-        callList(call, listener)
+        executeCall(remote.list(), listener)
     }
 
     fun listNext(listener: APIListener<List<TaskModel>>) {
-        val call = remote.listNext()
-        callList(call, listener)
+        executeCall(remote.listNext(), listener)
     }
 
     fun listOverduo(listener: APIListener<List<TaskModel>>) {
-        val call = remote.listOverduo()
-        callList(call, listener)
+        executeCall(remote.listOverduo(), listener)
     }
 
     fun create(task: TaskModel, listener: APIListener<Boolean>) {
         val call = remote.create(task.priorityId, task.description, task.dueDate, task.complete)
-        call.enqueue(object : Callback<Boolean> {
-            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-        })
-    }
-
-    // Método para fazer a chamada. Extraido para reutilização.
-    private fun callList(call: Call<List<TaskModel>>, listener: APIListener<List<TaskModel>>) {
-        call.enqueue(object : Callback<List<TaskModel>> {
-            override fun onResponse(
-                call: Call<List<TaskModel>>,
-                response: Response<List<TaskModel>>
-            ) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<List<TaskModel>>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-
-        })
+        executeCall(call, listener)
     }
 
     fun delete(id: Int, listener: APIListener<Boolean>) {
-        val call = remote.delete(id)
-        call.enqueue(object : Callback<Boolean>{
-            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-
-        })
+        executeCall(remote.delete(id), listener)
     }
 
 }
