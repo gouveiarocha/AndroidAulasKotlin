6a1f828405c25e35fcaf40e9376ef66c1ba924de
gouveiarocha <gouveia.rocha@hotmail.com>
2022-08-28 14:57:52 -0300

    Desafio do while.

---
+++ Udemy_Curso_AndroidKotlin/src/main/java/com/example/gouveiarocha/estudoskotlin/estudos/Kotlin/Secao7_ControleFluxo/13Desafio_DoWhile.kt
@@ -0,0 +1,27 @@
+package com.example.gouveiarocha.estudoskotlin.estudos.Kotlin.Secao7_ControleFluxo
+
+fun main() {
+
+    val salario = 10000f
+    var patrimonioAna = 0f
+    var patrimonioPaula = 0f
+    var mes = 0
+
+    do {
+
+        // Patrimonio da Ana
+        patrimonioAna += (salario * 0.05f) + (salario * 0.05f) + (patrimonioAna * 0.002f)
+
+        // Patrimonio da Paula
+        patrimonioPaula += (salario * 0.05f) + (patrimonioPaula * 0.008f)
+
+        mes++
+
+    } while (patrimonioAna > patrimonioPaula)
+
+    println("Patrimonio Ana: $patrimonioAna")
+    println("Patrimonio Paula: $patrimonioPaula")
+    println("Paula passa o patrimonio de Ana no mês $mes")
+    println("Anos que isso vai demorar: ${mes / 12}")
+
+}
