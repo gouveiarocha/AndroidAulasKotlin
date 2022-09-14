25fadd56c97a22c0f7d0c88d2004393553dfa5ad
gouveiarocha <gouveia.rocha@hotmail.com>
2022-09-13 22:00:32 -0300

    Estudo Elementos - Spinners

--- Udemy_Curso_AndroidKotlin/src/main/java/com/example/gouveiarocha/estudoskotlin/estudos/Elementos_Secoes_19_38/StudyElementsActivity2.kt
+++ Udemy_Curso_AndroidKotlin/src/main/java/com/example/gouveiarocha/estudoskotlin/estudos/Elementos_Secoes_19_38/StudyElementsActivity2.kt
@@ -4,18 +4,23 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.size
 import com.example.gouveiarocha.estudoskotlin.R
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_study_elements2.*
 
-class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener {
+class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener,
+    AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_elements2)
 
         initElements()
+        loadDynamicSpinner()
 
     }
 
@@ -23,7 +28,7 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener {
         when (v.id) {
 
             R.id.btn_toast_test -> {
-                toast("Teste Toast")
+                showToast("Teste Toast")
             }
             R.id.btn_toast_test_cust -> {
                 val toast = Toast.makeText(this, "Teste!!!", Toast.LENGTH_LONG)
@@ -44,12 +49,44 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener {
             R.id.btn_snack_test -> {
                 snack("Teste Snack!!!")
             }
+            R.id.btn_spinner_get_test -> {
+                val selectedItem = spinner_static_test.selectedItem
+                val selectedItemId = spinner_static_test.selectedItemId
+                val selectedItemPosition = spinner_static_test.selectedItemPosition
+                showToast("[Botões Testes] = Position: $selectedItemPosition - ID: $selectedItemId - Texto: $selectedItem")
+            }
+            R.id.btn_spinner_set_test -> {
+                // Só aceita o set pelo ID
+                spinner_static_test.setSelection(3)
+            }
 
         }
     }
 
-    private fun toast(str: String) {
-        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (parent?.id) {
+            R.id.spinner_static_test -> {
+                val selectedItem = parent.selectedItem
+                val selectedItemId = parent.selectedItemId
+                val selectedItemPosition = parent.selectedItemPosition
+                showToast("[OnItemSelect] = Position: $selectedItemPosition - ID: $selectedItemId - Texto: $selectedItem")
+            }
+            R.id.spinner_dynamic_test -> {
+                //Ações de seleção no Spinner Dinamico.
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        showToast("nothing")
+    }
+
+    /**
+     * ---> Métodos
+     */
+
+    private fun showToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 
     private fun snack(str: String) {
@@ -57,7 +94,7 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener {
         val snack = Snackbar.make(linear_root, str, Snackbar.LENGTH_LONG)
         // Cria ação no Snack.
         snack.setAction("Desfazer", View.OnClickListener {
-            toast("Desfeito...")
+            showToast("Desfeito...")
         })
         // Customiza cores.
         snack.setActionTextColor(Color.BLUE)
@@ -66,10 +103,22 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener {
         snack.show()
     }
 
-    private fun initElements(){
+    private fun initElements() {
+
         btn_toast_test.setOnClickListener(this)
         btn_toast_test_cust.setOnClickListener(this)
         btn_snack_test.setOnClickListener(this)
+        btn_spinner_get_test.setOnClickListener(this)
+        btn_spinner_set_test.setOnClickListener(this)
+
+        spinner_static_test.onItemSelectedListener = this
+
+    }
+
+    private fun loadDynamicSpinner() {
+        val mList = listOf("Gramas Spinner Dinamico", "Kg Spinner Dinamico", "Arroba Spinner Dinamico", "Tonelada Spinner Dinamico")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mList)
+        spinner_dynamic_test.adapter = adapter
     }
 
 }
