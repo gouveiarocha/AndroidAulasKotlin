72af3339126f45912b10c9f3a6ef2be102cb55bd
gouveiarocha <gouveia.rocha@hotmail.com>
2022-09-14 12:16:46 -0300

    Elementos Seekbar, Switch e CheckBox.

--- Udemy_Curso_AndroidKotlin/src/main/java/com/example/gouveiarocha/estudoskotlin/estudos/Elementos_Secoes_19_38/StudyElementsActivity2.kt
+++ Udemy_Curso_AndroidKotlin/src/main/java/com/example/gouveiarocha/estudoskotlin/estudos/Elementos_Secoes_19_38/StudyElementsActivity2.kt
@@ -4,22 +4,22 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gouveiarocha.estudoskotlin.R
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_study_elements2.*
 
 class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener,
-    AdapterView.OnItemSelectedListener {
+    AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener,
+    CompoundButton.OnCheckedChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_elements2)
 
         initElements()
         loadDynamicSpinner()
+
 
     }
 
@@ -57,9 +57,26 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener,
                 // Só aceita o set pelo ID
                 spinner_static_test.setSelection(3)
             }
+            R.id.btn_seekbar_get_test -> {
+                showToast(" Seekbar = ${seekbar_test.progress}")
+
+                // Seta o Max
+//                seekbar_test.max = 100
+
+                // Seta o Min
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                    seekbar_test.min = 10
+//                }
+
+            }
+            R.id.btn_seekbar_set_test -> {
+                seekbar_test.progress = 35
+            }
         }
     }
 
+    // Lembrete: O método onItemSelected), serve p tratar clique nos elementos diferentes, porém do
+    // mesmo tipo na nossa View - como é o exemplo do nosso spinner.
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         when (parent?.id) {
             R.id.spinner_static_test -> {
@@ -79,7 +96,49 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener,
     }
 
     /**
-     * ---> Métodos
+     * ---> Métodos Seekbar.
+     */
+
+    override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromUser: Boolean) {
+        text_seekbar_value_test.text = "Valor Seekbar = $progress"
+    }
+
+    override fun onStartTrackingTouch(seekbar: SeekBar?) {
+        showToast("Track started")
+    }
+
+    override fun onStopTrackingTouch(seekbar: SeekBar?) {
+        showToast("Track stoped")
+    }
+
+    /**
+     * ---> Método Switch e CheckBox
+     */
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        // O onCheckedChange pode observar mais elementos que retornam true ou false, daí a
+        // importancia de verificar com o When.
+        when (buttonView?.id) {
+            R.id.switch_test -> {
+                showToast("Switch ${if (isChecked) "True" else "False"}")
+                // Exemplos alterando o estado\propriedades do nosso Switch programaticamente.
+//                buttonView.isChecked = true
+//                buttonView.isEnabled = true
+//                buttonView.isClickable = true
+            }
+            R.id.checkbox_test -> {
+                showToast("CheckBox ${if (isChecked) "True" else "False"}")
+                buttonView.text = if (isChecked) "On" else "Off"
+                // Exemplos alterando o estado\propriedades do nosso Checkbox programaticamente.
+//                buttonView.isChecked = true
+//                buttonView.isEnabled = true
+//                buttonView.isClickable = true
+            }
+        }
+    }
+
+    /**
+     * ---> Métodos Proprios.
      */
 
     private fun showToast(str: String) {
@@ -104,11 +163,21 @@ class StudyElementsActivity2 : AppCompatActivity(), View.OnClickListener,
 
         btn_toast_test.setOnClickListener(this)
         btn_toast_test_cust.setOnClickListener(this)
+
         btn_snack_test.setOnClickListener(this)
+
         btn_spinner_get_test.setOnClickListener(this)
         btn_spinner_set_test.setOnClickListener(this)
 
         spinner_static_test.onItemSelectedListener = this
+
+        btn_seekbar_get_test.setOnClickListener(this)
+        btn_seekbar_set_test.setOnClickListener(this)
+
+        seekbar_test.setOnSeekBarChangeListener(this)
+
+        switch_test.setOnCheckedChangeListener(this)
+        checkbox_test.setOnCheckedChangeListener(this)
 
     }
 
