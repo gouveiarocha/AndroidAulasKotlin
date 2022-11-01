2ecf0e39f87d45e59bb6ce06fd429a2f91694dbb
gouveiarocha <gouveia.rocha@hotmail.com>
2022-11-01 16:56:44 -0300

    Toast and Snack Notification

--- YT-PDE_Android/src/main/java/com/gouveia/yt_pde_android/gdc/GdcHomeFragment.kt
+++ YT-PDE_Android/src/main/java/com/gouveia/yt_pde_android/gdc/GdcHomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import com.gouveia.yt_pde_android.R
 import com.gouveia.yt_pde_android.databinding.FragmentGdcHomeBinding
+import com.gouveia.yt_pde_android.util.navTo
 
 class GdcHomeFragment : androidx.fragment.app.Fragment(R.layout.fragment_gdc_home) {
 
@@ -11,7 +12,11 @@ class GdcHomeFragment : androidx.fragment.app.Fragment(R.layout.fragment_gdc_hom
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding = FragmentGdcHomeBinding.bind(view)
+
+        binding.codelabToastSnake.setOnClickListener { navTo(R.id.toastSnakeFragment) }
+
     }
 
 }
---
+++ YT-PDE_Android/src/main/java/com/gouveia/yt_pde_android/gdc/codelab/core/toast/ToastSnakeFragment.kt
@@ -0,0 +1,47 @@
+package com.gouveia.yt_pde_android.gdc.codelab.core.toast
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.gouveia.yt_pde_android.R
+import com.gouveia.yt_pde_android.databinding.FragmentToastSnakeBinding
+import com.gouveia.yt_pde_android.util.toast
+
+class ToastSnakeFragment : Fragment(R.layout.fragment_toast_snake) {
+
+    private lateinit var binding: FragmentToastSnakeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentToastSnakeBinding.bind(view)
+
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
+        // Toast Notification - Usando modo convencional.
+        binding.toast.setOnClickListener {
+            val toastText = "I am TOAST!"
+            Toast.makeText(requireContext(), toastText, Toast.LENGTH_SHORT).show()
+            //toast(msg) //exemplo usando extensions.
+        }
+
+        // Snackbar - Usando modo convencional.
+        binding.snake.setOnClickListener {
+            val snackText = "I am SNACK!"
+            Snackbar.make(view, snackText, Snackbar.LENGTH_SHORT).show()
+            //snake(view, snackText) //exemplo usando extensions.
+        }
+
+        // Snackbar com Ação
+        binding.snakeAction.setOnClickListener {
+            Snackbar.make(view, "Snack test with Action...", Snackbar.LENGTH_SHORT)
+                .setAction("OK") { toast("OK Clicked...") }
+                .show()
+        }
+
+    }
+
+}
