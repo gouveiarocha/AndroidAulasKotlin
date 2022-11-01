274699084c07b4baa1dbd7bd9845cce54e0ae902
gouveiarocha <gouveia.rocha@hotmail.com>
2022-11-01 17:20:54 -0300

    Simple Adjusts

---
+++ YT-PDE_Android/src/main/java/com/gouveia/yt_pde_android/dca/DcaHomeFragment.kt
@@ -0,0 +1,17 @@
+package com.gouveia.yt_pde_android.dca
+
+import android.os.Bundle
+import android.view.View
+import com.gouveia.yt_pde_android.R
+import com.gouveia.yt_pde_android.databinding.FragmentDcaHomeBinding
+
+class DcaHomeFragment : androidx.fragment.app.Fragment(R.layout.fragment_dca_home) {
+
+    private lateinit var binding: FragmentDcaHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDcaHomeBinding.bind(view)
+    }
+
+}
